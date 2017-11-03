ee0a17ad8f29a3e0a95f08c1e610140c7e27662f
Hack Reactor Student <student@hackreactor.com>
2017-11-02 19:58:29 -0700

    Add promises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,15 +4,33 @@
 
 var fs = require('fs');
 var request = require('request');
+var http = require('https');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function(err, contents) {
+    if (err) {  
+      callback(err, null);
+    } else {
+      const lines = contents.split("\n");
+      callback(err, lines[0]);
+    }
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  http.get(url, function(res) {
+    const {statusCode} = res;
+    let error;
+    if ({statusCode} !== 200) {
+      error = new Error('Request failed');
+      callback(error, `${statusCode}`);
+    } else {
+      callback(error, `${statusCode}`);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
